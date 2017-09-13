432eabf7599fb48321355492a1cc97c3a3419805
Dylan S <simondylan@hotmail.com>
2017-09-13 10:17:18 -0400

    New virtual battery data reformat
    creates the csv file into the column format csv

--- GUI_code/virtual_battery_data_reformat.m
+++ GUI_code/virtual_battery_data_reformat.m
@@ -1,85 +1,54 @@
 %%%%
 % Export all virtualBatteryData.mat to csv
 % First Row gives device name for the 4 columns (including the one with the
-% device name
+% device name(Ordered alphabetically)
 % Column 1: minPCapTotal
 % Column 2: maxPCapTotal
 % Column 3: minECapTotal
 % Column 4: maxECapTotal
+% ...
+% Column 20: maxECapTotal
 %%%%
 
-
+% function 
 
 load('virtualBatteryData.mat')
 
-ac = struct('minPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.ac(:)/1e6,...
-    'maxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.ac(:)/1e6,...
-    'minEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.ac(:)/1e6,...
-    'maxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.ac(:)/1e6);
+California_Struct = struct('ACminPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.ac(:)/1e6,...  %AC 
+    'ACmaxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.ac(:)/1e6,...  %AC 
+    'ACminEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.ac(:)/1e6,...  %AC
+    'ACmaxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.ac(:)/1e6,...  %AC 
+    'HPminPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.hp(:)/1e6,...  %HP
+    'HPmaxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.hp(:)/1e6,...  %HP
+    'HPminEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.hp(:)/1e6,...  %HP
+    'HPmaxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.hp(:)/1e6,...  %HP
+    'OFFICEminPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.office(:)/1e6,...  %OFFICE
+    'OFFICEmaxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.office(:)/1e6,...  %OFFICE
+    'OFFICEminEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.office(:)/1e6,...  %OFFICE
+    'OFFICEmaxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.office(:)/1e6,...  %OFFICE
+    'RGminPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.rg(:)/1e6,...  %RG
+    'RGmaxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.rg(:)/1e6,...  %RG
+    'RGminEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.rg(:)/1e6,...  %RG
+    'RGmaxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.rg(:)/1e6,...  %RG
+    'WHminPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.wh(:)/1e6,...  %WH
+    'WHmaxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.wh(:)/1e6,...  %WH
+    'WHminEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.wh(:)/1e6,...  %WH
+    'WHmaxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.wh(:)/1e6);    %WH
 
-ac = struct2array(ac);
+California = struct2array(California_Struct);
 
-hp = struct('minPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.hp(:)/1e6,...
-    'maxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.hp(:)/1e6,...
-    'minEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.hp(:)/1e6,...
-    'maxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.hp(:)/1e6);
+save('virtual_battery_data_reformat.mat','California_Struct');
 
-hp = struct2array(hp);
+fileID = fopen('virtual_battery_data_reformat.csv','w');
 
-office = struct('minPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.office(:)/1e6,...
-    'maxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.office(:)/1e6,...
-    'minEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.office(:)/1e6,...
-    'maxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.office(:)/1e6);
-
-office = struct2array(office);
-
-rg = struct('minPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.rg(:)/1e6,...
-    'maxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.rg(:)/1e6,...
-    'minEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.rg(:)/1e6,...
-    'maxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.rg(:)/1e6);
-
-rg = struct2array(rg);
-
-wh = struct('minPData',-virtualBatteryData(5).cap_60_minute.minPCapTotal.wh(:)/1e6,...
-    'maxPData',-virtualBatteryData(5).cap_60_minute.maxPCapTotal.wh(:)/1e6,...
-    'minEData',-virtualBatteryData(5).cap_60_minute.minECapTotal.wh(:)/1e6,...
-    'maxEData',-virtualBatteryData(5).cap_60_minute.maxECapTotal.wh(:)/1e6);
-
-wh = struct2array(wh);
-
-California = struct('ac',ac,'hp',hp,'office',office,'rg',rg,'wh',wh);
-
-save('virtualBatteryDataReformat.mat','California')
-
-fileID = fopen('virtualBatteryDataReformat.csv','w');
-
-fprintf(fileID,'%s\n','AC');
-fprintf(fileID,'%s, %s, %s, %s,\n','minPCapTotal','maxPCapTotal',...
-    'minECapTotal','maxECapTotal');
-fprintf(fileID,'%f, %f, %f, %f,\n',ac);
-
-fprintf(fileID,'%s\n','HP');
-fprintf(fileID,'%s, %s, %s, %s,\n','minPCapTotal','maxPCapTotal',...
-    'minECapTotal','maxECapTotal');
-fprintf(fileID,'%f, %f, %f, %f,\n',hp);
-
-fprintf(fileID,'%s\n','Office');
-fprintf(fileID,'%s, %s, %s, %s,\n','minPCapTotal','maxPCapTotal',...
-    'minECapTotal','maxECapTotal');
-fprintf(fileID,'%f, %f, %f, %f,\n',office);
-
-fprintf(fileID,'%s\n','RG');
-fprintf(fileID,'%s, %s, %s, %s,\n','minPCapTotal','maxPCapTotal',...
-    'minECapTotal','maxECapTotal');
-fprintf(fileID,'%f, %f, %f, %f,\n',rg);
-
-fprintf(fileID,'%s\n','WH');
-fprintf(fileID,'%s, %s, %s, %s,\n','minPCapTotal','maxPCapTotal',...
-    'minECapTotal','maxECapTotal');
-fprintf(fileID,'%f, %f, %f, %f,\n',wh);
+fprintf(fileID,'%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\n',...
+    'AC','','','','HP','','','','OFFICE','','','','RG','','','','WH','','','');
+fprintf(fileID,'%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\n',...
+    'minPCapTotal','maxPCapTotal','minECapTotal','maxECapTotal',...
+    'minPCapTotal','maxPCapTotal','minECapTotal','maxECapTotal',...
+    'minPCapTotal','maxPCapTotal','minECapTotal','maxECapTotal',...
+    'minPCapTotal','maxPCapTotal','minECapTotal','maxECapTotal',...
+    'minPCapTotal','maxPCapTotal','minECapTotal','maxECapTotal');
+fprintf(fileID,'%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,\n',California);
 
 fclose(fileID);
-
-% csvwrite('virtualBatteryDataReformat.csv',California)
-% csvwrite('virtualBatteryDataReformat.csv',California,1,0)
-% dlmwrite('virtualBatteryDataReformat.csv',California,'-append')%,1,0)
