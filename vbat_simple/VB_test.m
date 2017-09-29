a7c37b653a57ad76f1702c0b050a459038aa3605
Dylan S <simondylan@hotmail.com>
2017-09-29 13:05:13 -0400

    Small Fix

--- vbat_simple/VB_test.m
+++ vbat_simple/VB_test.m
@@ -2,10 +2,14 @@
 % VB_func(temperature,device#)
 % temperature is a csv containing a time stamp and a temperature
 
-VB_func('outdoor_temperature.csv',1,0) %AC
+% VB_func('outdoor_temperature.csv',1,0) %AC
 
 % VB_func('outdoor_temperature.csv',2,0) %HP
 
 % VB_func('outdoor_temperature.csv',3,0) %RG
 
 % VB_func('outdoor_temperature.csv',4,0) %WH
+
+%%% User inputed parameters
+
+% VB_func('outdoor_temperature.csv',2,'para_special.csv') 
