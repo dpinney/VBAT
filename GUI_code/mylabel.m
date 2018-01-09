06cb981a0626f9dd43513315507f90dce53bc6c2
Dylan S <simondylan@hotmail.com>
2018-01-09 16:08:00 -0500

    changing vbat files

--- GUI_code/mylabel.m
+++
@@ -1,9 +0,0 @@
-function mylabel(fig,evd,ax)
-currentax = evd.Axes; 
-if ~isempty(find(ax==currentax))
-datetick(currentax, 'x', 'keeplimits');
-ticks  = get(currentax, 'XTick');
-labels = get(currentax, 'XTickLabel');
-set(ax, 'XTick', ticks, 'XTickLabel', labels);
-end
-end
